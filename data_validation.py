6d23d8648bf5c368f6639d8f15a3d8965416800c
Jun Aishima <jaishima@bnl.gov>
2025-08-13 13:46:47 -0400

    fix print to work more generally
     * the only field we are guaranteed to get for testing is the uuid

--- data_validation.py
+++ data_validation.py
@@ -8,7 +8,7 @@ def read_all_streams(uid, beamline_acronym):
     logger = get_run_logger()
     tiled_client = from_profile("nsls2")
     run = tiled_client[beamline_acronym]["raw"][uid]
-    logger.info(f"Validating uid {run.start['uid']}")
+    logger.info(f"Validating uid {uid}")
     start_time = ttime.monotonic()
     for stream in run:
         logger.info(f"{stream}:")
