23d5228898541e9442d475cbf52f06419dbe483d
Jun Aishima <jaishima@bnl.gov>
2025-08-15 16:17:03 -0400

    add more logging to linker

--- linker.py
+++ linker.py
@@ -37,6 +37,7 @@ def do_symlinking(
 
     for uid, src, dest, analysis in tqdm.tqdm(links, leave=False):
         if not src.exists():
+            logger.error(f"{src} does not exist. uid: {uid} dest: {dest} analysis: {analysis}")
             failed.append((uid, src, dest, analysis))
             continue
 
