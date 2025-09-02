2e7569563e0d0dc8599c3719829013b5e725271d
Jun Aishima <jaishima@bnl.gov>
2025-09-02 08:25:18 -0400

    fix logging output - use exception and f string

--- linker.py
+++ linker.py
@@ -66,7 +66,7 @@ def do_symlinking(
 
         except Exception as e:
             tqdm.tqdm.write(f"FAILED: {dest}")
-            logger.error("Exception while making symlink: {src} to {dest}")
+            logger.exception(f"Exception while making symlink: {src} to {dest}")
             failed.append((uid, src, dest, analysis))
         else:
             tqdm.tqdm.write(f"Linked: {dest}")
