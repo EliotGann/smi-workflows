901548087d7b5ee8894c47ce285b0ad451f18bb8
Jun Aishima <jaishima@bnl.gov>
2025-09-02 08:17:34 -0400

    do not use concurrent running
     * attempt to fix incorrect results from workflows

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -12,7 +12,7 @@ def log_completion():
     logger.info("Complete")
 
 
-@flow(task_runner=ConcurrentTaskRunner())
+@flow
 def end_of_run_workflow(stop_doc):
     logger = get_run_logger()
     uid = stop_doc["run_start"]
@@ -20,16 +20,11 @@ def end_of_run_workflow(stop_doc):
 
     # Launch validation and linker concurrently.
     det_map = {"900KW": "WAXS", "1M": "SAXS", "2M": "SAXS2M"}
-    linker_task = get_symlink_pairs.submit(uid, det_map=det_map)
-    logger.info("Launched linker task")
-    validation_task = read_all_streams.submit(uid, beamline_acronym="smi")
-    logger.info("Launched validation task")
-    export_task = export_amptek.submit(uid)
-    logger.info("Launched amptek export task")
+    logger.info("Running linker task")
+    get_symlink_pairs(uid, det_map=det_map)
+    logger.info("Running validation task")
+    read_all_streams(uid, beamline_acronym="smi")
+    logger.info("Running amptek export task")
+    export_amptek(uid)
 
-    # Wait for completion.
-    logger.info("Waiting for tasks to complete")
-    validation_task.result()
-    linker_task.result()
-    export_task.result()
     log_completion()
