76ace79290bcb441ae49e1b039b35dad6cb3f1a5
Jun Aishima <jaishima@bnl.gov>
2026-03-05 13:52:12 -0500

    refactor to remove extra level of indirection
     * extract reading stream to a task

--- data_validation.py
+++ data_validation.py
@@ -12,20 +12,21 @@ def get_client(uid, api_key=None):
     return run
 
 
-@task(retries=2, retry_delay_seconds=10)
-def read_all_streams(uid, api_key=None):
+@task
+def read_stream(run, stream):
+    stream_data = run[stream].read()
+    return stream_data
+
+
+@flow
+def data_validation(uid, api_key=None):
     start_time = ttime.monotonic()
     for stream in run:
         logger.info(f"{stream}:")
         stream_start_time = ttime.monotonic()
-        stream_data = run[stream].read()
+        stream_data = read_stream(run, stream)
         stream_elapsed_time = ttime.monotonic() - stream_start_time
         logger.info(f"{stream} elapsed_time = {stream_elapsed_time}")
         logger.info(f"{stream} nbytes = {stream_data.nbytes:_}")
     elapsed_time = ttime.monotonic() - start_time
     logger.info(f"{elapsed_time = }")
-
-
-@flow
-def data_validation(uid, api_key=None):
-    read_all_streams(uid, api_key=api_key)
