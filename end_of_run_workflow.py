5ed83ace79f055bbbbbe9a36f72474c8c0988fcb
Jun Aishima <jaishima@bnl.gov>
2026-03-10 11:27:30 -0400

    remove exception handling for reading secret file
     * it should just fail
     * the two paths for getting an API key are:
       * value set in end_of_run_workflow() call
       * container.secret file

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -10,13 +10,9 @@ from dotenv import load_dotenv
 
 def get_api_key_from_env(api_key=None):
     logger = get_run_logger()
-    try:
-        with open("/srv/container.secret", "r") as secrets:
-            load_dotenv(stream=secrets)
-    except Exception:
-        logger.exception("Exception while getting Tiled API key")
-    finally:
-        api_key = os.environ["TILED_API_KEY"]
+    with open("/srv/container.secret", "r") as secrets:
+        load_dotenv(stream=secrets)
+    api_key = os.environ["TILED_API_KEY"]
     return api_key
 
 
